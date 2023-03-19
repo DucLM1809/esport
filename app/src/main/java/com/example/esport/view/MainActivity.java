4e20d6fed75e63ff899a6bb457989f14f60d3441
minhduc <minhductxqt@gmail.com>
2023-03-19 21:05:55 +0700

    payment checkout

--- app/src/main/java/com/example/esport/view/MainActivity.java
+++ app/src/main/java/com/example/esport/view/MainActivity.java
@@ -2,21 +2,80 @@ package com.example.esport.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.esport.R;
 import com.example.esport.presenter.UserPresenter;
 import com.google.android.material.textfield.TextInputEditText;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-public class MainActivity extends AppCompatActivity {
-    ImageView btnSignIn;
-    TextInputEditText etEmail, etPassword;
-    UserPresenter userPresenter;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PaymentResultListener  {
+    Button btnPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPayment = findViewById(R.id.btnPayment);
+
+        btnPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentMethod();
+            }
+        });
+    }
+
+    private void paymentMethod() {
+        Checkout checkout = new Checkout();
+
+        final Activity activity = MainActivity.this;
+
+        try {
+            JSONObject options = new JSONObject();
+            //Set Company Name
+            options.put("name", "My E-Commerce App");
+            //Ref no
+            options.put("description", "Reference No. #123456");
+            //Image to be display
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            //options.put("order_id", "order_9A33XWu170gUtm");
+            // Currency type
+            options.put("currency", "USD");
+            //double total = Double.parseDouble(mAmountText.getText().toString());
+            //multiply with 100 to get exact amount in rupee
+            //amount
+            options.put("amount", 1000);
+            JSONObject preFill = new JSONObject();
+            //email
+            preFill.put("email", "developer.kharag@gmail.com");
+            //contact
+            preFill.put("contact", "7489347378");
+
+            options.put("prefill", preFill);
+
+            checkout.open(activity, options);
+        } catch (Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Payment Successful", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, "Payment Cancel", Toast.LENGTH_SHORT).show();
     }
 }
